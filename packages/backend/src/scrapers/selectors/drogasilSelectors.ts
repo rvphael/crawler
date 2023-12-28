7b716131c0770e6c961ea122a2eff61674a7609e
Raphael Santos <raphaelsantos.j@gmail.com>
2023-12-28 14:53:48 -0300

    feat(scraper): add CSS selectors for Drogasil scraper

---
+++ packages/backend/src/scrapers/selectors/drogasilSelectors.ts
@@ -0,0 +1,7 @@
+export const drogasilSelectors = {
+  name: 'seletor-css-para-nome',
+  barcode: 'seletor-css-para-codigo-de-barras',
+  brand: 'seletor-css-para-marca',
+  image: 'seletor-css-para-imagem',
+  price: 'seletor-css-para-preço',
+};
