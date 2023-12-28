2e33fa524a5347457eeb0fa32ee8ef7e29b405a8
Raphael Santos <raphaelsantos.j@gmail.com>
2023-12-28 12:58:19 -0300

    feat(backend): implement error handling and request logging middlewares

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,9 +1,13 @@
 import express, { Express } from 'express';
+import errorHandler from './middlewares/errorHandler';
+import requestLogger from './middlewares/requestLogger';
 import routes from './routes';
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
+app.use(requestLogger);
 
 export default app;
---
+++ packages/backend/src/middlewares/errorHandler.ts
@@ -0,0 +1,12 @@
+import { ErrorRequestHandler } from 'express';
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+  });
+};
+
+export default errorHandler;
