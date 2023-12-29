e73b5f77f91f90217ed1d00f5df700588991e4c2
Raphael Santos <raphaelsantos.j@gmail.com>
2023-12-29 13:58:09 -0300

    chore: setup database test

--- packages/backend/src/config/config.js
+++ packages/backend/src/config/config.js
@@ -8,4 +8,11 @@ module.exports = {
     host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
   },
+  test: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_DATABASE || 'crawler_db_test',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql',
+  },
 };
