bd4c2d76a87b0e2c9a3772a63079d5df6512508e
Raphael Santos <raphaelsantos.j@gmail.com>
2023-12-29 13:57:25 -0300

    chore: add collect covarage

--- packages/backend/jest.config.js
+++ packages/backend/jest.config.js
@@ -2,4 +2,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@backend/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts",
+  ],
+  coverageDirectory: "coverage",
 };
