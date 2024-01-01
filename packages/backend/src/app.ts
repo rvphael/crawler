58a9e567ed40d26521d54e1d81f6d694cc82a61a
Raphael Santos <raphaelsantos.j@gmail.com>
2024-01-01 19:56:33 -0300

    feat: setup routing for product pages and correct issues with CORS and query parameter handling

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express, { Express } from 'express';
 import errorHandler from './middlewares/errorHandler';
 import requestLogger from './middlewares/requestLogger';
@@ -6,6 +7,11 @@ import routes from './routes';
 const app: Express = express();
 
 app.use(express.json());
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000'
+}));
+
 app.use(routes);
 app.use(errorHandler);
 app.use(requestLogger);
