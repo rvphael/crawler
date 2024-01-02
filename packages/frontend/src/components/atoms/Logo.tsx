e160af9b02bfcc198703b0ba0d4d48c8a822706c
Raphael Santos <raphaelsantos.j@gmail.com>
2024-01-02 13:20:48 -0300

    feat: integrate Pill logo image in Logo atom

--- packages/frontend/src/components/atoms/Logo.tsx
+++ packages/frontend/src/components/atoms/Logo.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Box } from '@mui/material';
 
 const Logo: React.FC = () => (
-  <Typography variant="h6">LOGO PILL</Typography>
+  <Box
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    width="fit-content"
+    padding="8px"
+    bgcolor="white"
+    borderRadius="4px"
+    boxShadow="0 2px 4px rgba(0,0,0,0.2)"
+  >
+    <img
+      src="https://pill.com.br/cdn/shop/t/51/assets/header-union.svg?v=4551551774730843711689454729"
+      alt="Logo Pill"
+      style={{ height: '40px' }}
+    />
+  </Box>
 );
 
 export default Logo;
