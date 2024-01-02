9ada1ee29717d9597a688358641b5d26f481bf12
Raphael Santos <raphaelsantos.j@gmail.com>
2024-01-02 13:22:27 -0300

    feat: finalize ProductNotFoundPage with error state handling

--- packages/frontend/src/pages/ProductNotFoundPage.tsx
+++ packages/frontend/src/pages/ProductNotFoundPage.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import Header from '../components/organisms/Header';
 import Footer from '../components/organisms/Footer';
-import { Typography } from '@mui/material';
+import Logo from '../components/atoms/Logo';
+import { useLocation } from 'react-router-dom';
+import { Box, Typography } from '@mui/material';
 
-const ProductNotFoundPage: React.FC = () => (
-  <div>
-    <Header />
-    <main style={{ padding: '2rem', textAlign: 'center' }}>
-      <Typography variant="h5">Produto não encontrado</Typography>
-    </main>
-    <Footer />
-  </div>
-);
+const ProductNotFoundPage: React.FC = () => {
+  const location = useLocation();
+  const error = location.state?.error || 'Produto não encontrado';
+
+  return (
+    <Box display="flex" flexDirection="column" minHeight="100vh" bgcolor="black">
+      <Header />
+      <Box flex={1} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+        <Logo />
+        <Box
+          mt={2}
+          width="60%"
+          border="1px solid white"
+          p={2}
+          mb={2}
+        >
+          <Typography variant="h5" color="white" textAlign="center">
+            {error}
+          </Typography>
+        </Box>
+      </Box>
+      <Footer />
+    </Box>
+  );
+};
 
 export default ProductNotFoundPage;
