e650cdaecb1834f18273d83f19e6ee236a6f9e66
Raphael Santos <raphaelsantos.j@gmail.com>
2024-01-02 13:21:25 -0300

    refactor: restructure ProductDetail molecule to include Image atom

--- packages/frontend/src/components/molecules/ProductDetail.tsx
+++ packages/frontend/src/components/molecules/ProductDetail.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
+import { Grid, Paper } from '@mui/material';
+import Name from '../atoms/Name';
+import BarcodeAndBrand from '../atoms/BarcodeAndBrand';
+import Price from '../atoms/Price';
+import Image from '../atoms/Image';
 
 interface ProductDetailProps {
   name: string;
   barCode: string;
   brand: string;
   price: string;
+  imageUrl: string;
 }
 
-const ProductDetail: React.FC<ProductDetailProps> = ({ name, barCode, brand, price }) => (
+const ProductDetail: React.FC<ProductDetailProps> = ({ name, barCode, brand, price, imageUrl }) => (
   <Grid container spacing={2}>
-    <Grid item xs={12}>
-      <Typography variant="h5">{name}</Typography>
+    <Grid item xs={12} md={6}>
+      <Image src={imageUrl} alt={name} />
     </Grid>
-    <Grid item xs={12}>
-      <Typography variant="body1">{barCode} - {brand}</Typography>
-    </Grid>
-    <Grid item xs={12}>
-      <Typography variant="h6">{price}</Typography>
+    <Grid item xs={12} md={6}>
+      <Paper style={{ border: '1px solid white', padding: '8px', backgroundColor: 'black', borderRadius: 0 }}>
+        <Name value={name} />
+      </Paper>
+      <Paper style={{ border: '1px solid white', padding: '8px', backgroundColor: 'black', borderRadius: 0, marginTop: '8px' }}>
+        <BarcodeAndBrand barcode={barCode} brand={brand} />
+      </Paper>
+      <Paper style={{ border: '1px solid white', padding: '8px', backgroundColor: 'black', borderRadius: 0, marginTop: '8px', display: 'flex', justifyContent: 'flex-end' }}>
+        <Price value={`R$ ${price}`} />
+      </Paper>
     </Grid>
   </Grid>
 );
