548dcf882e258fca29ff28bb413d77d0147fa3dc
Raphael Santos <raphaelsantos.j@gmail.com>
2024-01-03 11:56:56 -0300

    remove(selectors): delete obsolete drogasilSelectors file

--- packages/backend/src/scrapers/selectors/drogasilSelectors.ts
+++
@@ -1,7 +0,0 @@
-export const drogasilSelectors = {
-  name: 'h1[data-qa="seo-product_name-h1validator"]',
-  barcode: 'tr th:contains("EAN") + td > div',
-  brand: 'li.brand',
-  image: 'img.small-img',
-  price: 'div[data-qa="price_final_item"]',
-};
